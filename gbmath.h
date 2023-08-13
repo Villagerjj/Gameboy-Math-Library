4e3981eb9d3dea3ac46944e7fe2d5a0ffd6dbd05
Villagerjj <56765269+Villagerjj@users.noreply.github.com>
2023-08-13 18:31:06 -0400

    Update gbmath.h
    DO NOT USE THIS VERSION, IT DOES NOT HAVE THE LUTS!

--- gbmath.h
+++ gbmath.h
@@ -6,22 +6,23 @@
 
 #define TABLE_SIZE 256
 
-// Lookup tables for sin and cos values
-const int8_t sinTable[TABLE_SIZE] = {0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 59, 62, 65, 67, 70, 73, 75, 78, 80, 82, 85, 87, 89, 91, 94, 96, 98, 100, 102, 103, 105, 107, 108, 110, 112, 113, 114, 116, 117, 118, 119, 120, 121, 122, 123, 123, 124, 125, 125, 126, 126, 126, 126, 126, 127, 126, 126, 126, 126, 126, 125, 125, 124, 123, 123, 122, 121, 120, 119, 118, 117, 116, 114, 113, 112, 110, 108, 107, 105, 103, 102, 100, 98, 96, 94, 91, 89, 87, 85, 82, 80, 78, 75, 73, 70, 67, 65, 62, 59, 57, 54, 51, 48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15, 12, 9, 6, 3, 0, -3, -6, -9, -12, -15, -18, -21, -24, -27, -30, -33, -36, -39, -42, -45, -48, -51, -54, -57, -59, -62, -65, -67, -70, -73, -75, -78, -80, -82, -85, -87, -89, -91, -94, -96, -98, -100, -102, -103, -105, -107, -108, -110, -112, -113, -114, -116, -117, -118, -119, -120, -121, -122, -123, -123, -124, -125, -125, -126, -126, -126, -126, -126, -127, -126, -126, -126, -126, -126, -125, -125, -124, -123, -123, -122, -121, -120, -119, -118, -117, -116, -114, -113, -112, -110, -108, -107, -105, -103, -102, -100, -98, -96, -94, -91, -89, -87, -85, -82, -80, -78, -75, -73, -70, -67, -65, -62, -59, -57, -54, -51, -48, -45, -42, -39, -36, -33, -30, -27, -24, -21, -18, -15, -12, -9, -6, -3, };
-const int8_t cosTable[TABLE_SIZE] = {127, 126, 126, 126, 126, 126, 125, 125, 124, 123, 123, 122, 121, 120, 119, 118, 117, 116, 114, 113, 112, 110, 108, 107, 105, 103, 102, 100, 98, 96, 94, 91, 89, 87, 85, 82, 80, 78, 75, 73, 70, 67, 65, 62, 59, 57, 54, 51, 48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15, 12, 9, 6, 3, 0, -3, -6, -9, -12, -15, -18, -21, -24, -27, -30, -33, -36, -39, -42, -45, -48, -51, -54, -57, -59, -62, -65, -67, -70, -73, -75, -78, -80, -82, -85, -87, -89, -91, -94, -96, -98, -100, -102, -103, -105, -107, -108, -110, -112, -113, -114, -116, -117, -118, -119, -120, -121, -122, -123, -123, -124, -125, -125, -126, -126, -126, -126, -126, -127, -126, -126, -126, -126, -126, -125, -125, -124, -123, -123, -122, -121, -120, -119, -118, -117, -116, -114, -113, -112, -110, -108, -107, -105, -103, -102, -100, -98, -96, -94, -91, -89, -87, -85, -82, -80, -78, -75, -73, -70, -67, -65, -62, -59, -57, -54, -51, -48, -45, -42, -39, -36, -33, -30, -27, -24, -21, -18, -15, -12, -9, -6, -3, 0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 59, 62, 65, 67, 70, 73, 75, 78, 80, 82, 85, 87, 89, 91, 94, 96, 98, 100, 102, 103, 105, 107, 108, 110, 112, 113, 114, 116, 117, 118, 119, 120, 121, 122, 123, 123, 124, 125, 125, 126, 126, 126, 126, 126, };
+
 
 // Fast approximation of sine function using lookup table
-int8_t fast_sin(uint8_t angle) {
+int8_t fast_sin(uint8_t angle)
+{
     return sinTable[angle];
 }
 
 // Fast approximation of cosine function using lookup table
-int8_t fast_cos(uint8_t angle) {
+int8_t fast_cos(uint8_t angle)
+{
     return cosTable[angle];
 }
 
 // Fast approximation of tangent function using lookup tables
-int8_t fast_tan(uint8_t angle) {
+int8_t fast_tan(uint8_t angle)
+{
     // Calculate tangent as sin(angle) / cos(angle)
     return (fast_sin(angle) << 8) / fast_cos(angle);
 }
@@ -30,22 +31,64 @@ int8_t fast_tan(uint8_t angle) {
 #define GB_FLOAT_FRACTIONAL_BITS 8
 typedef int16_t GBfloat;
 
-// Basic arithmetic operations
-static inline GBfloat GBfloatAdd(GBfloat a, GBfloat b) {
-    return a + b;
+// Basic arithmetic operations for the GBfloat variable type
+static inline GBfloat GBfloatAdd(GBfloat a, GBfloat b)
+{
+    int32_t result = a + b;
+    int16_t integerPart = result >> GB_FLOAT_FRACTIONAL_BITS;
+    int16_t fractionalPart = result & 0xFF;
+
+    if (fractionalPart >= 100 || fractionalPart <= -100)
+    {
+        integerPart += fractionalPart / 100;
+        fractionalPart %= 100;
+    }
+
+    return (GBfloat)((integerPart << GB_FLOAT_FRACTIONAL_BITS) + fractionalPart);
 }
 
-static inline GBfloat GBfloatSub(GBfloat a, GBfloat b) {
-    return a - b;
+static inline GBfloat GBfloatSub(GBfloat a, GBfloat b)
+{
+    int32_t result = a - b;
+    int16_t integerPart = result >> GB_FLOAT_FRACTIONAL_BITS;
+    int16_t fractionalPart = result & 0xFF;
+
+    if (fractionalPart >= 100 || fractionalPart <= -100)
+    {
+        integerPart += fractionalPart / 100;
+        fractionalPart %= 100;
+    }
+
+    return (GBfloat)((integerPart << GB_FLOAT_FRACTIONAL_BITS) + fractionalPart);
 }
 
-
-static inline GBfloat GBfloatMultiply(GBfloat a, GBfloat b) {
+static inline GBfloat GBfloatMultiply(GBfloat a, GBfloat b)
+{
     int32_t result = ((int32_t)a * b + (1 << (GB_FLOAT_FRACTIONAL_BITS - 1))) >> GB_FLOAT_FRACTIONAL_BITS;
     int16_t integerPart = result >> GB_FLOAT_FRACTIONAL_BITS;
-    uint8_t fractionalPart = result & 0xFF;
+    int16_t fractionalPart = result & 0xFF;
 
-    if (fractionalPart > 99) {
+    if (fractionalPart >= 100 || fractionalPart <= -100)
+    {
+        integerPart += fractionalPart / 100;
+        fractionalPart %= 100;
+    }
+
+    return (GBfloat)((integerPart << GB_FLOAT_FRACTIONAL_BITS) + fractionalPart);
+}
+
+static inline GBfloat GBfloatDivide(GBfloat a, GBfloat b)
+{
+    int32_t result = ((int32_t)a << GB_FLOAT_FRACTIONAL_BITS) / b;
+    int16_t integerPart = result >> GB_FLOAT_FRACTIONAL_BITS;
+    int16_t fractionalPart = result & 0xFF;
+
+    // Round the fractional part
+    fractionalPart = (fractionalPart * 100 + b / 2) / b;
+
+    // Check if the fractional part is greater or equal to 100
+    if (fractionalPart >= 100)
+    {
         integerPart++;
         fractionalPart -= 100;
     }
@@ -53,22 +96,23 @@ static inline GBfloat GBfloatMultiply(GBfloat a, GBfloat b) {
     return (GBfloat)((integerPart << GB_FLOAT_FRACTIONAL_BITS) + fractionalPart);
 }
 
-static inline GBfloat GBfloatDivide(GBfloat a, GBfloat b) {
-    return (GBfloat)((((int32_t)a << GB_FLOAT_FRACTIONAL_BITS) / b));
-}
-
 // Conversion function
-static inline GBfloat floatToGBfloat(int8_t whole_number, uint8_t decimal) {
+static inline GBfloat floatToGBfloat(int8_t whole_number, uint8_t decimal)
+{
     int16_t result;
-    if (whole_number < 0) {
+    if (whole_number < 0)
+    {
         result = -((abs(whole_number) << GB_FLOAT_FRACTIONAL_BITS) + (decimal & 0xFF));
-    } else {
+    }
+    else
+    {
         result = (whole_number << GB_FLOAT_FRACTIONAL_BITS) + (decimal & 0xFF);
     }
     return result;
 }
 
-static inline int8_t GBfloatToInt8(GBfloat value) {
+static inline int8_t GBfloatToInt8(GBfloat value)
+{
     return (int8_t)(value >> GB_FLOAT_FRACTIONAL_BITS);
 }
 
